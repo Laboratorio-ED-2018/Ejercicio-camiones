9853f3c78678ff5224147e6e1ba7b5f50741aca5
Inaki Cabo <icaboalo@gmail.com>
2018-09-03 13:58:22 -0500

    Se agregaron los últimos dos ejercicios

--- src/proyecto/Business.java
+++ src/proyecto/Business.java
@@ -15,6 +15,10 @@ import java.util.Scanner;
 public class Business {
     Truck[] trucks = new Truck[10];
     
+    
+    /*
+     * imprimir todos los datos de todos los camiones de pasajeros, 
+     */
     public void printPassengers() {
         StringBuilder sb = new StringBuilder();
         for (Truck camion : trucks) {
@@ -26,6 +30,9 @@ public class Business {
         System.out.println(sb.toString());
     }
     
+    /*
+     * imprimir todos los datos de todos los camiones de volteo
+     */
     public void printCargo() {
         StringBuilder sb = new StringBuilder();
         for (Truck camion : trucks) {
@@ -37,6 +44,10 @@ public class Business {
         System.out.println(sb.toString());
     }
     
+    /*
+     * dada la placa de un camión de volteo, actualizar su capacidad de 
+     * transporte (el usuario indicará la nueva capacidad)
+     */
     public void changeCapacity(String plates) {
         for (Truck truck : trucks) {
             if (truck instanceof Cargo && truck.getPlates().equals(plates)) {
@@ -47,5 +58,36 @@ public class Business {
         }
     }
     
+    /*
+     * calcular e imprimir la cantidad total de camiones de pasajeros que sean de una marca
+     * específica (el usuario indicará cuál),
+     */
+    public void printPassengerTrucksByBrand() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("¿Cuál es la marca?");
+        String brand = scan.next();
+        int count = 0;
+        for (Truck truck : trucks) {
+            if (truck instanceof Passengers && truck.getBrand().equals(brand)) {
+                count ++;
+            }
+        }
+        System.out.println("Camiones de pasajeros de la marca " + brand + ": " + count);
+    }
     
+    /*
+     * calcular e imprimir la cantidad total de
+     * toneladas de tierra que puede ser transportada simultáneamente (por todos los camiones
+     * de volteo de la empresa).
+     */
+    public void printTotalCargo() {
+        int count = 0;
+        for (Truck truck : trucks) {
+            if (truck instanceof Cargo) {
+                count += ((Cargo) truck).getCargo();
+            }
+        }
+        
+        System.out.println("Cantidad total de toneladas: " + count);
+    }
 }
